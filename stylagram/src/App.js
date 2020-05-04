8dadfe87d2af97bcb4356cf6a0b235e2251d5d4e
ferueda <fe.rueda@outlook.com>
2020-05-04 14:49:16 -0400

    finished Stylagram excercise

--- stylagram/src/App.js
+++ stylagram/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './components/Header';
 import Profile from './components/Profile';
+import GlobalStyle from './theme/globalStyle';
+
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: palevioletred;
+  color: ${({ primary }) => (primary ? 'violet' : 'palevioletred')};
+  border: ${({ primary }) =>
+    primary ? '2px solid violet' : '2px solid palevioletred'};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+
+  &:hover {
+    color: white;
+    background-color: ${({ primary }) =>
+      primary ? 'violet' : 'palevioletred'};
+  }
+`;
+
+const GreenButton = styled(Button)`
+  background-color: white;
+  color: green;
+  border-color: green;
+`;
+
+const AppWrapper = styled.div`
+  background-color: #fafafa;
+`;
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Profile />
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <AppWrapper>
+        <Header />
+        <Profile />
+      </AppWrapper>
+    </React.Fragment>
   );
 }
 
